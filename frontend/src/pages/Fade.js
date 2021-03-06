3a49c714d7ddfa46e26523d3b7a4f6045745f007
Farhan Mahmood <zcabfm0@ucl.ac.uk>
2021-03-06 20:36:07 +0000

    Rearrange ratings breakdown on movies page

---
+++ frontend/src/pages/Fade.js
@@ -0,0 +1,22 @@
+import { useEffect, useRef, useState } from "react";
+
+function FadeInSection(props) {
+  const [isVisible, setVisible] = useState(false);
+  const domRef = useRef();
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+  }, []);
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? "is-visible" : ""}`}
+      ref={domRef}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default FadeInSection;
--- frontend/src/pages/Movie.js
+++ frontend/src/pages/Movie.js
@@ -6,6 +6,7 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Ratings from "../Components/Movie/Ratings";
 import "./Movie.css";
+import Fade from "./Fade.js";
 
 function Movie() {
   const { id } = useParams();
@@ -37,33 +38,47 @@ function Movie() {
   if (dataLoaded) {
     return (
       <div className="body">
-        <CustomNavbar />
-        <div className="poster-container">
-          <div
-            style={{
-              backgroundImage: "url(" + movie.movieURL + ")",
-            }}
-            className="bg-image"
-          ></div>
+        <div className="body">
+          <CustomNavbar />
+          <div className="poster-container">
+            <div
+              style={{
+                backgroundImage: "url(" + movie.movieURL + ")",
+              }}
+              className="bg-image"
+            ></div>
+          </div>
+          <Container className="content" fluid>
+            <Row style={{ width: "100%" }}>
+              <Col sm={1} md={2} lg={3} />
+              <Col sm={4} md={4} lg={3}>
+                <img className="movie-movie-image" src={movie.movieURL} />
+              </Col>
+              <Col sm={7} md={6}>
+                <p className="movie-movie-title">{movie.title}</p>
+              </Col>
+            </Row>
+          </Container>
         </div>
-        <Container className="content" fluid>
-          <Row style={{ width: "100%" }}>
-            <Col sm={1} md={2} lg={3} />
-            <Col sm={4} md={4} lg={3}>
-              <img className="movie-movie-image" src={movie.movieURL} />
-            </Col>
-            <Col sm={7} md={6}>
-              <p className="movie-movie-title">{movie.title}</p>
-            </Col>
-          </Row>
-          <Row style={{ width: "100%", paddingTop: "20px" }}>
-            <Col xs={3}></Col>
-            <Col xs={6}>
-              <Ratings movieID={movieID} />
-            </Col>
-            <Col xs={3}></Col>
-          </Row>
-        </Container>
+        <div className="ratings-menu">
+          <Fade>
+            <div
+              style={{
+                display: "flex",
+                alignContent: "center",
+                height: "100%",
+              }}
+            >
+              <Row style={{ width: "100%", paddingTop: "20px" }}>
+                <Col xs={2}></Col>
+                <Col xs={8}>
+                  <Ratings movieID={movieID} />
+                </Col>
+                <Col xs={2}></Col>
+              </Row>
+            </div>
+          </Fade>
+        </div>
       </div>
     );
   }
